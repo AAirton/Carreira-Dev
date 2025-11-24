13ec2735a3fc365ac09a6b3566572a07ba01eb82
Daniel N. Rebouças <danielnreboucas@hotmail.com>
2025-11-23 22:27:45 -0300

    Adicionando Problemas para Segunda-feira

---
+++ Estudos/Semana-1/Segunda/Problema-2.py
@@ -0,0 +1,26 @@
+'''
+Problema 2) 
+
+Sem executar este código pelo VSCode ou python
+Execute usando uma folha de Papel e diga o que
+Irá aparecer no Print abaixo.
+'''
+
+a = 10
+b = 3
+
+a = a + b * 2
+c = 2 + a 
+d = a + b + c
+
+e = 0
+if d > 10:
+    e = 1
+else:
+    e = 2
+
+f = e * 10
+g = f + c
+g = f + g
+
+print(a, b, c, d, e, f, g)
