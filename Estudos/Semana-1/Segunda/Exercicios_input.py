4641faf17427087d44f1967d5f4984d43db7f2a3
Airton F. Junior <aairtonjunior0@gmail.com>
2025-11-24 16:27:34 -0300

    Adicionado exercicios_1 do Curso

---
+++ Estudos/Semana-1/Segunda/Exercicios_input.py
@@ -0,0 +1,58 @@
+"""
+EXERCICIO 01 - Apresentação Pessoal
+
+Crie um programa em Python, que solicite três informações ao usuário
+Nome, idade e cidade e no final, o codigo deve imprimir da seguinte forma
+
+Olá, Leonardo! Voce tem 29 anos e mora na cidade de São Paulo!
+"""
+# print('Qual o seu nome: ')
+# nome = input()
+# print('Qual a sua idade: ')
+# idade = input()
+# print('Qual a sua cidade: ')
+# cidade = input()
+
+# print('Olá,', nome,'! Você tem',idade,'anos e mora na cidade de',cidade,'!') 
+
+
+"""
+EXERCICIO 02 - Cadastro de aluno
+
+Crie um programa em python, que ira pedir para que um aluno digite
+Nome, o nome do curso que está fazendo e digitar o periodo que pode ser
+manhã, tarde ou noite ,e o programa deve imprimir no final
+uma mensagem informando que o aluno foi cadastrado com sucesso e as informações digitadas
+"""
+# print('Cadastro de aluno.')
+# print('Qual o seu nome: ')
+# nome = input()
+# print('Qual o seu curso: ')
+# curso = input()
+# print('Qual turno você deseja se matricular: ')
+# turno = input()
+
+# print('Olá', nome,'. Parabês, você acabou de ser matriculado no curso de', curso,'e o seu turno será', turno)
+
+
+"""
+EXERCICIO 03 -  Simulação de reserva de viagem
+Crie um programa em Python que irá pedir para que o usuário digite:
+
+Seu nome
+
+O destino da viagem que ele deseja fazer
+
+Ao final, o programa deve imprimir uma mensagem personalizada informando que a reserva foi registrada com sucesso, junto com as informações fornecidas.
+
+Exemplo de saída esperada:
+Olá, João!
+Sua reserva para Salvador foi registrada com sucesso.
+
+"""
+# print('Qual o seu nome: ')
+# nome = input()
+# print('Para qual destino você deseja viajar: ')
+# destino = input()
+
+# print('Olá,', nome,'.Sua reserva para', destino,'foi registrada com sucesso!')
---
+++ Estudos/Semana-1/Segunda/Notas.md
@@ -0,0 +1,13 @@
+# Fundamentos da programação em **python**
+
+##Variável
+1. Tipos de variável:
+- _print(type())_
+1.1. Char - **str**
+1.2. Numérica - **int**  
+1.3. Ponto flutuante - **float**
+1.4. Booleana <true/false> - **bool**
+
+
+
+
